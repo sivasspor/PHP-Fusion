272d2fe97581c4fc6413201b2853717e92b26d47
sivasspor <yusufulku58@hotmail.com>
2015-12-15 10:38:33 +0200

    Update error.php

--- locale/Turkish/error.php
+++ locale/Turkish/error.php
@@ -4,4 +4,4 @@ $locale['err403'] = "Access forbidden to this page!";
 $locale['err404'] = "This page was not found on this server!";
 $locale['err500'] = "Internal server error!";
 $locale['errunk'] = "Unknown error";
-$locale['errret'] = "Return to main site";
+$locale['errret'] = "Ana siteye dön";
